77635dbf89adf072b34ae582b721446fda1f0763
mxfm1 <1f.andresM@gmail.com>
2025-12-15 05:40:16 -0300

    add:añadir invitaciones referentes a la residencia

--- invitations/urls.py
+++ invitations/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("create",InvitationCreate.as_view()),
+    path("user/all/",Invitations.as_view(),name="invitation-list"),
+    path("user/invitations/",Invitations.as_view(),name="invitation-new-list"),
     path("all/",AllInvitations.as_view(),name="all-invitations")
 ]
