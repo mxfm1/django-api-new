abdecd28067e51cacb42b2cbb15beb6effe3cc8b
mxfm1 <1f.andresM@gmail.com>
2025-12-15 05:11:56 -0300

    delete conflict routes

--- invitations/urls.py
+++ invitations/urls.py
@@ -3,7 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("create",InvitationCreate.as_view()),
-    path("user/all/",Invitations.as_view(),name="invitation-list"),
-    path("user/invitations/",Invitations.as_view(),name="invitation-new-list"),
-    path("all",AllInvitations.as_view(),name="all-invitations")
+    path("all/",AllInvitations.as_view(),name="all-invitations")
 ]
