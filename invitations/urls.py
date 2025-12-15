668dca81f3c0412969783292503c659edb9d718c
mxfm1 <1f.andresM@gmail.com>
2025-12-15 03:32:09 -0300

    chore add user invitations listing

--- invitations/urls.py
+++ invitations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("create",InvitationCreate.as_view())
+    path("create",InvitationCreate.as_view()),
+    path("user/all",Invitations.as_view(),name="invitation-list")
 ]
--- invitations/views.py
+++ invitations/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView,ListAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from .serializers import InvitationSerializer
+from .models import Invitation
 
 # Create your views here.
 class InvitationCreate(CreateAPIView):
@@ -12,4 +13,23 @@ class InvitationCreate(CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(
             host = self.request.user
+        )
+
+class Invitations(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = InvitationSerializer
+    def get_queryset(self):
+        user = self.request.user
+
+        # obtener residencia del usuario
+        try:
+            residence = user.resident_profile.residence
+        except:
+            return Invitation.objects.none()
+
+        if not residence:
+            return Invitation.objects.none()
+
+        return Invitation.objects.filter(
+            residence=residence
         )
